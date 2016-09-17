068b4ed598c39ec549dda62436c411ef120f8104
Christian Rauch <Rauch.Christian@gmx.de>
2016-09-17 16:02:37 +0100

    cleanup of unused reading with timeout

--- src/SerialPort.cpp
+++ src/SerialPort.cpp
@@ -7,7 +7,7 @@
 
 using namespace boost::asio;
 
-SerialPort::SerialPort(const std::string &device) : port(io), timer(io) {
+SerialPort::SerialPort(const std::string &device) : port(io) {
     port.open(device);
 
     port.set_option(serial_port::baud_rate(115200));
@@ -15,8 +15,6 @@ SerialPort::SerialPort(const std::string &device) : port(io), timer(io) {
     port.set_option(serial_port::character_size(serial_port::character_size(8)));
     port.set_option(serial_port::stop_bits(serial_port::stop_bits::one));
 //    port.set_option(serial_port::flow_control(serial_port::flow_control::none));
-
-    io.run();
 }
 
 SerialPort::~SerialPort() {
@@ -40,68 +38,4 @@ std::vector<uint8_t> SerialPort::read(std::size_t n_bytes) {
 
 uint8_t SerialPort::read() {
     return read(1).front();
-}
-
-size_t SerialPort::read(std::vector<uint8_t> &data, uint64_t time_ms) {
-    io.reset();
-    //timer.cancel();
-    timer.expires_from_now(boost::posix_time::milliseconds(time_ms));
-    timer.async_wait(boost::bind(&SerialPort::timer_handler, this, boost::asio::placeholders::error));
-
-    size_t bt = 0;
-    bool success = false;
-
-    boost::asio::async_read(port, boost::asio::buffer(data.data(), data.size()), boost::bind(&SerialPort::read_handler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred, std::ref(bt), std::ref(success)));
-
-
-    std::cout<<"run done"<<std::endl;
-
-    while(success==false) {}
-    std::cout<<"done"<<std::endl;
-
-    return bt;
-}
-
-bool SerialPort::isOpen() {
-    return port.is_open();
-}
-
-void SerialPort::setDTR(bool flag) {
-    int fd = port.native_handle();
-
-    int data = TIOCM_DTR;
-    if (!flag)
-        ioctl(fd, TIOCMBIC, &data);
-    else
-        ioctl(fd, TIOCMBIS, &data);
-}
-
-void SerialPort::timer_handler(const boost::system::error_code& error) {
-    std::cout<<"time error: "<<error<<std::endl;
-
-    if(error)
-        std::cout<<"error"<<std::endl;
-
-    if(error == boost::asio::error::operation_aborted) {
-        std::cout<<"timer cancled"<<std::endl;
-    }
-    else {
-        std::cout<<"time passed"<<std::endl;
-        port.cancel();
-    }
-}
-
-void SerialPort::read_handler(const boost::system::error_code& error, std::size_t bytes_transferred, size_t &bt, bool &success) {
-    std::cout<<"read error: "<<error<<std::endl;
-    std::cout<<"bytes transferred: "<<bytes_transferred<<std::endl;
-
-    if(error == boost::asio::error::operation_aborted) {
-        std::cout<<"aborted"<<std::endl;
-    }
-
-    if(error==0 && bytes_transferred>0)
-        timer.cancel();
-
-    success = true;
-    bt = bytes_transferred;
 }
--- src/SerialPort.hpp
+++ src/SerialPort.hpp
@@ -5,31 +5,45 @@
 
 class SerialPort {
 public:
+    /**
+     * @brief SerialPort
+     * @param device path to serial device
+     */
     SerialPort(const std::string &device);
+
     ~SerialPort();
 
+    /**
+     * @brief write write data vector to device
+     * @param data raw data vector
+     * @return true on success
+     * @return false on failure
+     */
     bool write(const std::vector<uint8_t> &data);
 
+    /**
+     * @brief read read data vector from device
+     * @param data raw data vector in which read data will be stored
+     * @return number of read bytes
+     */
     size_t read(std::vector<uint8_t> &data);
 
+    /**
+     * @brief read read given amount of bytes from device
+     * @param n_bytes number of bytes to read
+     * @return data vector with read bytes
+     */
     std::vector<uint8_t> read(std::size_t n_bytes);
 
+    /**
+     * @brief read read a single byte from device
+     * @return single byte
+     */
     uint8_t read();
 
-    size_t read(std::vector<uint8_t> &data, uint64_t time_ms);
-
-    bool isOpen();
-
-    void setDTR(bool flag);
-
 private:
-    boost::asio::io_service io;
-    boost::asio::serial_port port;
-    boost::asio::deadline_timer timer;
-
-    void timer_handler(const boost::system::error_code& error);
-
-    void read_handler(const boost::system::error_code& error, std::size_t bytes_transferred, size_t &bt, bool &success);
+    boost::asio::io_service io;     ///<! io service
+    boost::asio::serial_port port;  ///<! port for serial device
 };
 
 #endif // SERIALPORT_HPP
