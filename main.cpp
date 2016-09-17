0fe42bd27e25c031cdc6648a6cbce36863b09ebe
Christian Rauch <Rauch.Christian@gmx.de>
2016-09-17 16:23:22 +0100

    wait (block) for controller to become ready and test update rate

--- main.cpp
+++ main.cpp
@@ -1,79 +1,46 @@
 #include <iostream>
 #include <MSP.hpp>
 
-#include <unistd.h>
 #include <chrono>
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello World!" << std::endl;
+    std::string device;
+    if(argc>1)
+        device = std::string(argv[1]);
+    else
+        device = "/dev/ttyUSB0";
 
-    msp::MSP msp("/dev/ttyUSB0");
+    msp::MSP msp(device);
     msp.setWait(1);
 
-    //sleep(10);
-
-    std::cout<<"ready"<<std::endl;
-
-    bool success;
-
     // try connecting until first package is received
+    {
+    std::cout<<"Waiting for flight controller to become ready..."<<std::endl;
+    auto start = std::chrono::steady_clock::now();
     msp::Ident ident;
-    do {
-        try {
-            success = msp.request(ident);
-            std::cout<<"success: "<<success<<std::endl;
-        }
-        catch(boost::system::system_error &e) {
-            //std::cerr<<"Cannot reach MSP yet. "<<e.what()<<std::endl;
-        }
+    msp.request_block(ident);
+    auto end = std::chrono::steady_clock::now();
 
-    } while(success==false);
+    std::cout<<"MSP version "<<(int)ident.version<<" ready after: "<<std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()<<" ms"<<std::endl;
+    }
 
-    std::cout<<"version: "<<(int)ident.version<<std::endl;
+    // test update rate for reading Gyro messages
+    {
+    const unsigned int max_msg = 1000;
+    unsigned int n_msg = 0;
+    auto start = std::chrono::steady_clock::now();
+    while(n_msg!=max_msg) {
+        msp::RawImu status;
+        msp.request_block(status);
+        n_msg++;
+    }
+    auto end = std::chrono::steady_clock::now();
 
-    msp::RcTuning rc_tuning;
-    do {
-        success = msp.request(rc_tuning);
-    } while(success==false);
-    std::cout<<"success: "<<success<<std::endl;
-    std::cout<<"thr expo: "<<(int)rc_tuning.Throttle_EXPO<<std::endl;
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
 
-    msp::SetRcTuning set_rc_tuning(rc_tuning);
-    set_rc_tuning.Throttle_EXPO = 73;
-    do {
-        success = msp.respond(set_rc_tuning);
-    } while(success==false);
-    std::cout<<"success: "<<success<<std::endl;
-
-    msp::RcTuning rc_tuning2;
-    do {
-        success = msp.request(rc_tuning2);
-    } while(success==false);
-    std::cout<<"success: "<<success<<std::endl;
-    std::cout<<"thr expo: "<<(int)rc_tuning2.Throttle_EXPO<<std::endl;
-
-
-//    auto start = std::chrono::steady_clock::now();
-//    unsigned int n_msg = 0;
-//    while(n_msg!=500) {
-//        //msp::Status status;
-//        msp::RawImu status;
-//        try {
-//            success = msp.request(status);
-//        }
-//        catch(boost::system::system_error &e) {
-//            //std::cerr<<"No data. "<<e.what()<<std::endl;
-//        }
-
-//        if(success) {
-//            //std::cout<<"time: "<<status.time<<" us"<<std::endl;
-//            success = false;
-//            n_msg++;
-//        }
-//    }
-//    auto end = std::chrono::steady_clock::now();
-
-//    std::cout<<"duration: "<<std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()<<" ms"<<std::endl;
+    std::cout<<"read "<<max_msg<<" messages in: "<<duration<<" ms"<<std::endl;
+    std::cout<<"messages per second: "<<max_msg/(duration/1000.0)<<" Hz"<<std::endl;
+    }
 
     return 0;
 }
