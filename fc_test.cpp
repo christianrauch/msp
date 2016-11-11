fd99675525e892342b029b8a207462a8cc1acd0d
Christian Rauch <Rauch.Christian@gmx.de>
2016-11-11 00:36:45 +0000

    subscribe to all requests in high-level API

--- fc_test.cpp
+++ fc_test.cpp
@@ -14,12 +14,12 @@ public:
         this->name = name;
     }
 
-    void onIdentFCU(const fcu::Ident* const ident) {
+    void onIdent(const fcu::Ident* const ident) {
         std::cout<<"Name: "<<name<<std::endl;
         std::cout<<(*ident);
     }
 
-    void onStatusFCU(const fcu::Status *status) {
+    void onStatus(const fcu::Status *status) {
         std::cout<<"Name: "<<name<<std::endl;
         std::cout<<(*status);
     }
@@ -34,6 +34,66 @@ public:
     void onImu(const fcu::Imu* const imu) {
         std::cout<<"Name: "<<name<<std::endl;
         std::cout<<(*imu);
+    }
+
+    void onServo(const msp::Servo* const servo) {
+        std::cout<<(*servo);
+    }
+
+    void onMotor(const msp::Motor* const motor) {
+        std::cout<<(*motor);
+    }
+
+    void onRc(const msp::Rc* const rc) {
+        std::cout<<(*rc);
+    }
+
+    void onAttitude(const fcu::Attitude* const attitude) {
+        std::cout<<(*attitude);
+    }
+
+    void onAltitude(const fcu::Altitude* const altitude) {
+        std::cout<<(*altitude);
+    }
+
+    void onAnalog(const fcu::Analog* const analog) {
+        std::cout<<(*analog);
+    }
+
+    void onRcTuning(const fcu::RcTuning* const rc_tuning) {
+        std::cout<<(*rc_tuning);
+    }
+
+    void onPID(const fcu::PID* const pid) {
+        std::cout<<(*pid);
+    }
+
+    void onBox(const fcu::Box* const box) {
+        std::cout<<(*box);
+    }
+
+    void onMisc(const fcu::Misc* const misc) {
+        std::cout<<(*misc);
+    }
+
+    void onMotorPins(const msp::MotorPins* const motor_pins) {
+        std::cout<<(*motor_pins);
+    }
+
+    void onBoxNames(const msp::BoxNames* const box_names) {
+        std::cout<<(*box_names);
+    }
+
+    void onPidNames(const msp::PidNames* const pid_names) {
+        std::cout<<(*pid_names);
+    }
+
+    void onBoxIds(const msp::BoxIds* const box_ids) {
+        std::cout<<(*box_ids);
+    }
+
+    void onServoConf(const msp::ServoConf* const servo_conf) {
+        std::cout<<(*servo_conf);
     }
 };
 
@@ -51,9 +111,27 @@ int main(int argc, char *argv[]) {
     fcu.setGyroUnit(1.0/4096);
 
     App app("MultiWii");
-    fcu.subscribe(msp::ID::MSP_IDENT, &App::onIdentFCU, &app);
-    fcu.subscribe(msp::ID::MSP_STATUS, &App::onStatusFCU, &app);
+    fcu.subscribe(msp::ID::MSP_IDENT, &App::onIdent, &app);
+    fcu.subscribe(msp::ID::MSP_STATUS, &App::onStatus, &app);
     fcu.subscribe(msp::ID::MSP_RAW_IMU, &App::onImu, &app);
+    fcu.subscribe(msp::ID::MSP_SERVO, &App::onServo, &app);
+    fcu.subscribe(msp::ID::MSP_MOTOR, &App::onMotor, &app);
+    fcu.subscribe(msp::ID::MSP_RC, &App::onRc, &app);
+    // TODO: RawGPS
+    // TODO: CompGPS
+    fcu.subscribe(msp::ID::MSP_ATTITUDE, &App::onAttitude, &app);
+    fcu.subscribe(msp::ID::MSP_ALTITUDE, &App::onAltitude, &app);
+    fcu.subscribe(msp::ID::MSP_ANALOG, &App::onAnalog, &app);
+    fcu.subscribe(msp::ID::MSP_RC_TUNING, &App::onRcTuning, &app);
+    fcu.subscribe(msp::ID::MSP_PID, &App::onPID, &app);
+    fcu.subscribe(msp::ID::MSP_BOX, &App::onBox, &app);
+    fcu.subscribe(msp::ID::MSP_MISC, &App::onMisc, &app);
+    fcu.subscribe(msp::ID::MSP_MOTOR_PINS, &App::onMotorPins, &app);
+    fcu.subscribe(msp::ID::MSP_BOXNAMES, &App::onBoxNames, &app);
+    fcu.subscribe(msp::ID::MSP_PIDNAMES, &App::onPidNames, &app);
+    // TODO: WayPoint
+    fcu.subscribe(msp::ID::MSP_BOXIDS, &App::onBoxIds, &app);
+    fcu.subscribe(msp::ID::MSP_SERVO_CONF, &App::onServoConf, &app);
 
     while(true) {
         fcu.handle();
