8165af2f96d76a8c4aac5b8a7a9de5ed282bdf95
Christian Rauch <Rauch.Christian@gmx.de>
2016-11-28 23:16:32 +0000

    example of arming and disarming
    The FC will go into failsafe mode between arming and disarming due to
    missing RC commands. Hence, motors will be active for short period of time.

---
+++ examples/fcu_arm_test.cpp
@@ -0,0 +1,48 @@
+#include <FlightController.hpp>
+
+#include <iostream>
+#include <chrono>
+
+
+int main(int argc, char *argv[]) {
+    std::string device;
+    if(argc>1)
+        device = std::string(argv[1]);
+    else
+        device = "/dev/ttyUSB0";
+
+    std::chrono::high_resolution_clock::time_point start, end;
+
+    fcu::FlightController fcu(device);
+
+    // wait until connection is established
+    start = std::chrono::high_resolution_clock::now();
+    fcu.waitForConnection();
+    end = std::chrono::high_resolution_clock::now();
+    std::cout<<"ready after: "<<std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()<<" ms"<<std::endl;
+
+    // get unique box IDs
+    fcu.initBoxes();
+
+    std::cout<<"Armed? "<<fcu.isArmed()<<std::endl;
+
+    // arm the FC
+    std::cout<<"Arming..."<<std::endl;
+    start = std::chrono::high_resolution_clock::now();
+    fcu.arm_block();
+    end = std::chrono::high_resolution_clock::now();
+
+    if(fcu.isArmed()) {
+        std::cout<<"armed after: "<<std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()<<" ms"<<std::endl;
+    }
+
+    // disarm the FC
+    std::cout<<"Disarming..."<<std::endl;
+    start = std::chrono::high_resolution_clock::now();
+    fcu.disarm_block();
+    end = std::chrono::high_resolution_clock::now();
+
+    if(!fcu.isArmed()) {
+        std::cout<<"disarmed after: "<<std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()<<" ms"<<std::endl;
+    }
+}
