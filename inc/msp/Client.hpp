0d5a390629759c943851179e223fc6ae5c95eb6c
Christian Rauch <Rauch.Christian@gmx.de>
2017-08-12 23:26:09 +0100

    move PeriodicTimer to Client

--- inc/msp/Client.hpp
+++ inc/msp/Client.hpp
@@ -8,7 +8,53 @@
 #include <map>
 
 #include "types.hpp"
-#include "PeriodicTimer.hpp"
+
+namespace msp {
+
+class PeriodicTimer {
+public:
+    typedef unsigned int uint;
+
+    /**
+     * @brief PeriodicTimer define a periodic timer
+     * @param funct function that is called periodically
+     * @param period_seconds period in seconds
+     */
+    PeriodicTimer(const std::function<void()> funct, const double period_seconds);
+
+    /**
+     * @brief start define and start background thread
+     */
+    void start();
+
+    /**
+     * @brief stop tell thread to stop and wait for end
+     */
+    void stop();
+
+    /**
+     * @brief getPeriod get period in seconds
+     * @return period in seconds
+     */
+    double getPeriod() {
+        return period_us.count()/1.e6;
+    }
+
+    /**
+     * @brief setPeriod change the update period of timer thread
+     * This will stop and restart the thread.
+     * @param period_seconds period in seconds
+     */
+    void setPeriod(const double period_seconds);
+
+private:
+    std::shared_ptr<std::thread> thread_ptr;
+    std::function<void()> funct;
+    std::chrono::duration<uint, std::micro> period_us;
+    bool running;
+};
+
+} // namespace msp
 
 namespace msp {
 namespace client {
